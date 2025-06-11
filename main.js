5cebbc0e6b770368d71708509e1c6daf86b2b8f1
igorguz89 <igorguz42@gmail.com>
2025-06-11 15:27:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+import { UserManager } from "https://cdn.jsdelivr.net/npm/oidc-client-ts@2.0.3/+esm";
+import { userManager, signOutRedirect } from './oidc-config.js';
+
+export { userManager, signOutRedirect };
