5588ab30e92f10df288678717e1b6bf43bba2a73
igorguz89 <igorguz42@gmail.com>
2025-06-11 15:43:59 +0200

    Update oidc-config.js

--- oidc-config.js
+++ oidc-config.js
@@ -1,10 +1,9 @@
-
-import { UserManager } from "https://cdn.jsdelivr.net/npm/oidc-client-ts@2.0.3/+esm";
+import { UserManager } from "oidc-client-ts";
 
 const cognitoAuthConfig = {
     authority: "https://cognito-idp.eu-north-1.amazonaws.com/eu-north-1_3pvC4DEG1",
     client_id: "22j935nr3cusdmb2vshjrimvj8",
-    redirect_uri: "https://main.d2rsrvfuodba1d.amplifyapp.com/callback.html",
+    redirect_uri: "https://main.d1o7orvs7bwkpi.amplifyapp.com/callback.html",
     response_type: "code",
     scope: "email openid phone"
 };
@@ -16,7 +15,7 @@ export const userManager = new UserManager({
 
 export async function signOutRedirect () {
     const clientId = "22j935nr3cusdmb2vshjrimvj8";
-    const logoutUri = "https://main.d2rsrvfuodba1d.amplifyapp.com/index.html";
+    const logoutUri = "https://main.d1o7orvs7bwkpi.amplifyapp.com/index.html";
     const cognitoDomain = "https://eu-north-13pvc4deg1.auth.eu-north-1.amazoncognito.com";
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
-}
+};
