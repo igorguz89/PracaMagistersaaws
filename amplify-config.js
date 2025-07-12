b3e311c923efbb8a791e0dd6ed0648f4b6e2078e
igorguz89 <igorguz42@gmail.com>
2025-07-12 21:31:22 +0200

    Add files via upload

--- amplify-config.js
+++ amplify-config.js
@@ -2,14 +2,15 @@
 // Skopiuj i wklej tutaj całą zawartość swojego oryginalnego pliku `aws-exports.js`.
 
 const awsExports = {
-  // ====================================================================
-  // PASTE THE CONTENT OF YOUR `aws-exports.js` FILE HERE
-  // For example:
-   "aws_project_region": "eu-north-1",
-   "aws_cognito_region": "eu-north-1",
-   "aws_user_pools_id": "eu-north-1_3pvC4DEG1",
-  // ... etc.
-  // ====================================================================
+  // Te wartości są wymagane do poprawnego działania Amplify z Cognito.
+  // Znajdziesz je w konsoli AWS w usłudze Cognito.
+  "aws_project_region": "eu-north-1",
+  "aws_cognito_region": "eu-north-1",
+  "aws_user_pools_id": "eu-north-1_3pvC4DEG1",
+  "aws_user_pools_web_client_id": "22j935nr3cusdmb2vshjrimvj8",
+  "oauth": {}, // Ten obiekt jest często wymagany, nawet jeśli jest pusty.
+  // Jeśli używasz Identity Pools, dodaj również ten klucz:
+  // "aws_cognito_identity_pool_id": "eu-north-1:xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
 };
 
 // Udostępniamy konfigurację globalnie dla innych skryptów.
